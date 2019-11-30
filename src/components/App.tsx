47aec3f9a2b744344ade453699734c396f3aeec2
Julian Callin <j.callin@hotmail.com>
2019-11-29 21:47:53 -0800

    Initial setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../../media/logo.svg";
+import "../scss/components/App.scss";
 
 const App: React.FC = () => {
   return (
@@ -21,6 +21,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
